2cd9cd811b8d5a582db2787b0504f56762b87a2c
KRISHNA KUAMR <krk5512@gmail.com>
2017-04-16 16:35:36 +0530

    Add files via upload

---
+++ fadeeffects.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class fadeeffects : MonoBehaviour
+{
+
+    public Text undeadText;
+    public float fadeSpeed = 5f;
+    public bool entrance;
+    public GameObject television;
+
+    void Awake() 
+
+    {
+
+        undeadText = television.GetComponentInChildren<Text>();
+        undeadText.color = Color.clear;
+
+    }
+
+    void Update()
+
+    {
+        ColorChange();
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            entrance = true;
+        }
+
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            entrance = false;
+
+        }
+
+    }
+
+    void ColorChange()
+
+    {
+
+        if (entrance)
+        {
+            undeadText.color = Color.Lerp(undeadText.color, Color.white, fadeSpeed * Time.deltaTime);
+
+        }
+
+        if (!entrance)
+        {
+            undeadText.color = Color.Lerp(undeadText.color, Color.clear, fadeSpeed * Time.deltaTime);
+        }
+
+    }
+}
